90761999daff789c804a0f88130b1f263fc3e75a
Sojaded <kelvin.kaweru@strathmore.edu>
2023-12-11 16:40:26 -0800

    0x18. C

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - mod of two integers
+ * @a: first int
+ * @b: second int
+ * Return: final ans
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - multiply of two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
